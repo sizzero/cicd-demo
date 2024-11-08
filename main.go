8184158b513331ba6ba1d9b6bf61dcf8b0888b33
liuzhenwen <liuzhenwen@zuoyebang.com>
2024-11-08 23:04:54 +0800

    ci init

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// 首页处理函数
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Go Server!")
+}
+
+// 处理 /hello 的请求并返回一个JSON响应
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"message": "Hello, World!"}`)
+}
+
+// 注册路由
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/hello", helloHandler)
+	log.Println("Server is starting on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func main() {
+	handleRequests()
+}
